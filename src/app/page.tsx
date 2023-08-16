a57dc44e4a4cceac7ba45a8f7a87c8ba26805045
Eric Sison <ericsison.dev@gmail.com>
2023-08-17 00:34:46 +0800

    removed default root page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const BasicMapLoader = dynamic(
-  () => import("@ericsison/mapspot/components/maps/basic-map/view/BasicMap").then((component) => component.BasicMap),
-  {
-    loading: () => <>Loading...</>,
-    ssr: false,
-  },
-);
-
-export default function Home() {
-  return (
-    <main className="h-screen w-screen flex items-center justify-center">
-      <BasicMapLoader />
-    </main>
-  );
+export default function Index() {
+  return <>Index</>;
 }
