a4af52353f5335cb62dbf9938b1ab1b93de35d9c
Eric Sison <ericsison.dev@gmail.com>
2023-08-23 17:28:44 +0800

    modified page to test queryClient and dev tools integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import { useQueryClientDevTools } from "../hooks/store/use-query-client-dev-tools-store";
+
 export default function Index() {
-  return <>Index</>;
+  const showDevTools = useQueryClientDevTools((state) => state.showDevTools);
+  const setShowDevTools = useQueryClientDevTools((state) => state.setShowDevTools);
+
+  return (
+    <>
+      <button onClick={() => setShowDevTools(!showDevTools)}>toggle dev tools on prod</button>
+    </>
+  );
 }
