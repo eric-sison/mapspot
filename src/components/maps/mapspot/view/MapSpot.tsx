ebf5a01d552c10b1451cd8bc57cf8945ca34845f
Eric Sison <ericsison.dev@gmail.com>
2023-08-23 08:45:20 +0800

    removed unnecesary lines

--- src/components/maps/mapspot/view/MapSpot.tsx
+++ src/components/maps/mapspot/view/MapSpot.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { FunctionComponent } from "react";
-import { stringify } from "flatted";
 import { OpenLayerMap, useMap } from "../../olm";
 import { LayerSwitcher } from "@ericsison/mapspot/components/controls/layer-switcher/views/LayerSwitcher";
-import mapConfig from "../../../../../config/map-config.json";
 
 export const MapSpot: FunctionComponent = () => {
   const { map, defaultLayers } = useMap();
-
-  console.log(mapConfig.defaultLayers);
 
   return (
     <OpenLayerMap
